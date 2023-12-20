b4b6553a763c027582fceaa4b5f4f7c1f8a0787e
FilipNedovic <filip.nedovic@epam.com>
2023-12-20 15:12:26 +0100

    util functions minor refactor

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,39 +1,37 @@
 import * as constants from "../fixtures/constants";
 
 const getTestId = (index) => {
-  let hostName = getHostName();
-  console.log(hostName)
-  return `${constants.BRANDS[hostName].toUpperCase()}-0${index}:`;
-};
+    let hostName = getHostName();
+    console.log(hostName);
+    return `${constants.BRANDS[hostName].toUpperCase()}-0${index}:`;
+  },
+  getHostName = () => {
+    let host = Cypress.env("host");
+    host = host.replace(".com", "");
 
-const getHostName = () => {
-  let host = Cypress.env("host");
-  host = host.replace(".com", "");
+    return host
+      .replace(/www.|staging.|.preview|.prod|https:\/\/|http:\/\//g, "")
+      .toUpperCase();
+  },
+  //prepare data to assert without special characters and whitespaces
+  prepareData = (pageData, csData) => {
+    return [
+      pageData.replace(/[^a-zA-Z ]|\s/g, ""),
+      csData.replace(/[^a-zA-Z ]|\s/g, ""),
+    ];
+  },
+  extractTextFromCSExport = (html) => {
+    let temp = document.createElement("div");
+    temp.innerHTML = html;
 
-  return host.replace(/www.|staging.|.preview|.prod|https:\/\/|http:\/\//g, "").toUpperCase();
-};
-
-//prepare data to assert without special characters and whitespaces
-const prepareData = (pageData, csData) => {
-  return [
-    pageData.replace(/[^a-zA-Z ]|\s/g, ""),
-    csData.replace(/[^a-zA-Z ]|\s/g, ""),
-  ];
-};
-
-const extractTextFromCSExport = (html) => {
-  let temp = document.createElement("div");
-  temp.innerHTML = html;
-
-  return temp.textContent || temp.innerText;
-};
-
-const getSanitizedData = (pageData, csData) => {
-  return prepareData(
-    extractTextFromCSExport(pageData),
-    extractTextFromCSExport(csData)
-  );
-};
+    return temp.textContent || temp.innerText;
+  },
+  getSanitizedData = (pageData, csData) => {
+    return prepareData(
+      extractTextFromCSExport(pageData),
+      extractTextFromCSExport(csData)
+    );
+  };
 
 export default {
   getTestId,
